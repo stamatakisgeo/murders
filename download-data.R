c2098801ce65b6a3b24ac0b43ed8c9de5d890466
stamatakisgeo <stamatakis@fleming.gr>
2021-04-13 16:48:28 +0300

    first connection

---
+++ download-data.R
@@ -0,0 +1,3 @@
+url<- "https://raw.githubusercontent.com/rafalab/dslabs/master/inst/extdata/murders.csv"
+dast_file<- "data/murders.csv"
+download.file(url, destfile = dast_file)
---
+++ downloadfile.R
@@ -0,0 +1,3 @@
+url<- "https://raw.githubusercontent.com/rairizarry/murders/master/report.Rmd"
+dast_file<- "data/report.Rmd"
+download.file(url, destfile = dast_file)
